591baa16c073d0175c0a3bbdf82f108b44e476d2
imran <imranisa1453@icloud.com>
2026-07-05 23:07:12 +0300

    sensor packet için struct olusturuldu

--- AquilaTelemetrySim/AquilaTelemetrySim.cpp
+++ AquilaTelemetrySim/AquilaTelemetrySim.cpp
@@ -1,6 +1,16 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+enum Sensor{derinlik,batarya,GPS};
+
+struct SensorPacket {
+    int id;  // 0 = derinlik, 1 = batarya, 2 = GPS
+    float value; // olculen deger
+    long timestamp; // degerin olculdugu zaman
+};
+
+
 
 int main()
 {
